5415988a56b95917d661930fed71b20e7787df00
Prashanth Rajagopal <prashanth@browserstack.com>
2014-06-06 13:32:02 +0530

    discounts read from config

--- lib/discount.rb
+++ lib/discount.rb
@@ -1,28 +1,27 @@
-# require 'active_support/core_ext/string'
+require 'yaml'
 require_relative './user.rb'
-
-EXEMPTED_LIST = ["chocolate"]
 
 class Discount
   attr_accessor :user, :item, :price
   def initialize args
+    @d_config = YAML.load_file('./discount.yml')
     @user, @item, @price = args
     @price = @price.to_i
   end
 
-	def calculate
+  def calculate
     v_discount = 0
-		p_discount = []
-		if EXEMPTED_LIST.include? @item.downcase
+    p_discount = []
+    if @d_config["exempted"].include? @item.downcase
       return 0
     else
-			p_discount << 30 if @user.employee == "true"
-			p_discount << 10 if @user.affiliate == "true"
-			p_discount << 5 if @user.loyal_customer
-      if @price >= 100
-        v_discount = @price / 100 * 5
+      p_discount << @d_config["employee"].to_i if @user.employee.to_s == "true"
+      p_discount << @d_config["affiliate"].to_i if @user.affiliate.to_s == "true"
+      p_discount << @d_config["loyal_customer"].to_i if @user.loyal_customer
+      if @price >= @d_config["step_discount"]["steps"].to_i
+        v_discount = (@price / 100) * @d_config["step_discount"]["discount"].to_i
       end
       return p_discount.min + v_discount
-		end
-	end
+    end
+  end
 end
